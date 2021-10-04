c38ef3e99e19e9c01b32c5eeb3f7fab01046b4e1
Mariano Jalif <marigyt.maj@gmail.com>
2021-10-04 12:30:48 -0400

    Deleted user_story.txt and added class Reader

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import sys
+from pathlib import Path
+
+class Reader:
+    def __init__(self, directory, files=None, calendar=None):
+        self.directory = directory
+        self.files = files
+        self.calendar = calendar
+    
+    def load_files(self, directory):
+        files = {}
+        for child in Path(directory).iterdir():
+            if child.is_file():
+                files[child.name] = child.read_text()
+        return files
+
 def main():
-    print("Hello world! This is Kyler and Chris and Mariano. Lets see if I can commit.")
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python3 main.py <filename>")
+        
 
 if __name__ == "__main__":
     main()
