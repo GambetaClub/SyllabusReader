8cd9745d2a5659bdb15ac4afb914aba6dbdf1740
Mariano Jalif <marigyt.maj@gmail.com>
2021-10-05 21:18:24 -0400

    Added filenames getter_function and some type checks for files and directory.

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ class Reader:
         self.__calendar = calendar
 
     def set_directory(self, directory):
+        if not isinstance(self.__directory, str):
+            raise TypeError("Directory must be a str")
         self.__directory = directory
 
     def get_directory(self):
-        if self.__directory is None:
-            raise RuntimeError("The object doesn't have a directory'")
         return self.__directory
 
+    def get_filenames(self):
+        return [key for key in self.__files.keys()]
+
     def set_files(self, files):
+        if not isinstance(files, dict):
+            raise TypeError("Files must be a dict")
         self.__files = files
 
     def get_files(self):
@@ -29,18 +34,25 @@ class Reader:
     def load_files(self, directory=None):
         if directory != None:
             self.set_directory(directory)
-        directory = self.get_directory()
         files = dict()
-        for filename in os.listdir(directory):
-            files[filename] = docx2txt.process(os.path.join(directory, filename))
+        for filename in os.listdir(self.get_directory()):
+            files[filename] = docx2txt.process(os.path.join(self.get_directory(), filename))
         self.set_files(files)
+
 
 def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python3 main.py <directory_name>")
     reader = Reader(sys.argv[1])
-    files = reader.load_files()
-    print(reader.get_files())
+
+    reader.load_files()
+
+    filenames = reader.get_filenames() # ['Syllabus1.docx', 'Syllabus2.docx', 'Syllabus3.docx']
+    
+    files = reader.get_files() # { 'Syllabus1.docx': "...", 'Syllabus2.docx': "...", ...}
+
+    print(files[filenames[0]])
+
 
 if __name__ == "__main__":
     main()
