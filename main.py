83c6b1ee9ee27d0d392ee04ff490fe7f63363177
Mariano Jalif <marigyt.maj@gmail.com>
2021-10-16 03:46:06 -0400

    Cleaned code a lot and improved latency. Still shows an error which I know what it is due. If it was done what suggested, the program wouldn't work.

--- main.py
+++ main.py
@@ -32,6 +32,18 @@ class Reader:
     def get_calendar(self):
         return self.__calendar
 
+    def check_date(self, d):
+        """
+        Takes a string date and returns
+        the a stringuniform date format if
+        possible. Otherwise, it returns None 
+        """
+        try:
+            d = dateutil.parser.parse(d)
+            return d
+        except:
+            return None
+
     def spec(self, s):
         """
         Takes a string. Returns False is the string is
@@ -43,40 +55,40 @@ class Reader:
         else:
             return None
 
-    def convert_assignments(self,df):
+    def check_syllabi(self):
         """
-        Converts the dataframe assignments 
-        values to None if the value is either
-        only spaces or symbol characters. 
+        Set the value of syllabi to be 
+        a dictionary of the valid of the 
+        documents with a valid format.
         """
+        syllabi = self.get_syllabi()
+        new_syllabi = dict()
+        for key, value in syllabi.items():
+            if value is None:
+                continue
+            new_syllabi[key] = value
+        self.set_syllabi(new_syllabi)
 
-        df['Assignments'] = df['Assignments'].map(lambda s: self.spec(s))
-        df = df[df.Assignments.notnull()]
-        return df
-
-    def convert_dates(self):
+    def convert_dates(self, df):
         """
         Convert the dates of the syllabi's
         dataframes into a monotonous format
         and sets the new dictionary for the 
         object attribute.
         """
-        syllabi = self.get_syllabi()
-        new_syllabi = dict()
-        for syllabus in syllabi:
-            if syllabi[syllabus] is None:
-                continue
-            else:
-                df = syllabi[syllabus]
-                for i, date in enumerate(df["Date"]):
-                    try:
-                        df["Date"][i] = dateutil.parser.parse(date)
-                    except:
-                        df["Date"][i] = None
+        df['Date'] = df['Date'].map(lambda d: self.check_date(d))
+        df = df[df.Date.notnull()]
+        return df 
 
-                df = df[df.Date.notnull()]
-                new_syllabi[syllabus] = df
-        self.set_syllabi(new_syllabi)    
+    def convert_assignments(self,df):
+        """
+        Converts the dataframe assignments 
+        values to None if the value is either
+        only spaces or symbol characters. 
+        """
+        df['Assignments'] = df['Assignments'].map(lambda s: self.spec(s))
+        df = df[df.Assignments.notnull()]
+        return df
 
     def recognize_fields(self, df):
         """
@@ -133,11 +145,12 @@ class Reader:
                 document = Document(os.path.join(sys.argv[1], filename))
                 syllabi[os.path.splitext(filename)[0]] = self.read_docx_table(document)
         self.set_syllabi(syllabi)
-        self.convert_dates()
+        self.check_syllabi()
 
         syllabi = self.get_syllabi()
         for syllabus in syllabi:
             df = syllabi[syllabus]
+            df = self.convert_dates(df)
             df = self.convert_assignments(df)
             syllabi[syllabus] = df
         self.set_syllabi(syllabi)
