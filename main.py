64c6c98f9af7ad0b394d317c471c128a7f7872ec
Mariano Jalif <marigyt.maj@gmail.com>
2021-10-18 13:00:04 -0400

    Nothing meaningful done.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 from ics_converter import ICSConverter
 from reader import Reader
-# Changing something...
+
 def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python3 main.py <directory_name>")
