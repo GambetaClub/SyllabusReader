f26ee12991b28da14d7edbb7cdaf41483a7149f2
Mariano Jalif <marigyt.maj@gmail.com>
2021-10-18 10:58:58 -0400

    Cleaned and removed prints useless for the user.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sys
-from pdf_date_finder import PDFDateFinder
 
 from ics_converter import ICSConverter
-from reader import Reader
+from Reader import Reader
 
 def main():
     if len(sys.argv) != 2:
@@ -13,27 +12,14 @@ def main():
 
     reader.load_syllabi()
     syllabi = reader.get_syllabi()
-    for syllabus in syllabi:
-        syllabi[syllabus].to_csv(f"{syllabus}.csv", index=False)
 
     #creating a ics file from csv files
     converter = ICSConverter(sys.argv[1])
-
-    converter.readCSV(f"Syllabus1.csv")
-    converter.readCSV(f"Syllabus2.csv")
+    for syllabus in syllabi:
+        syllabi[syllabus].to_csv(f"{syllabus}.csv", index=False)
+        converter.readCSV(f"{syllabus}.csv")
 
     converter.exportICS()
-
-    #reading a pdf
-    pdf_date_finder = PDFDateFinder()
-    pdf_date_finder.pdf2txt('syllabus1.pdf', 'syllabus1.txt')
-    
-    findDatesTuple = pdf_date_finder.findDates("syllabus1.txt")
-    print("find dates finished")
-    dates = findDatesTuple[0]
-    lines = findDatesTuple[1]
-    for date, line in zip(dates, lines):
-        print(line, date)
 
 if __name__ == "__main__":
     main()  
