1a67cfc10d12e1e1250f4cde9cab107b36d92522
ChrisHemstock <chris.hemstock@myemail.indwes.edu>
2021-10-31 01:02:00 -0400

    add events to calendar with an array of events

--- calendar/script.js
+++ calendar/script.js
@@ -1,8 +1,43 @@
 //Heavily inspired by Code and Create https://www.youtube.com/watch?v=o1yMqPyYeAo&t=1135s
+class CalendarEvent {
+
+    constructor(date, group, description) {
+        this.setDate(date)
+        this.setGroup(group);
+        this.setDescription(description);
+    }
+   
+    setDate(date) {
+        let dateArray = date.split('/')
+        dateArray[0] = dateArray[0] - 1;
+        this.date = dateArray;
+    }
+
+    setGroup(group) {
+        this.group = group;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    getDate(index) {
+        return this.date[index];
+    }
+
+    getGroup() {
+        return this.group;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+    
+}
+
 const date = new Date();
 
-
-const renderCalendar = () => {
+const renderCalendar = (eventArray) => {
     
     date.setDate(1);
 
@@ -48,11 +83,27 @@ const renderCalendar = () => {
 
     //displays the days
     for(let i = 1; i <= lastDay; i++){
+        let dailyEvents = "";
+        //true if its todays date
         if(i === new Date().getDate() && (date.getMonth() === new Date().getMonth()) && date.getFullYear() === new Date().getFullYear()) {
-            days += `<div class = "today">${i + " " + `<p>${"ev3nt"}</p>`}</div>`;
+            //loops through the event array
+            eventArray.forEach(function(event) {
+                //true if the event matches todays date
+                if(event.getDate(1) == i && (event.getDate(0) == date.getMonth() && event.getDate(2) == date.getFullYear())) {
+                        dailyEvents += event.getDescription() + "<br>";
+                }
+            })
+            days += `<div class = "today">${i + " " + `<p>${dailyEvents}</p>`}</div>`;
             
         }else{
-            days += `<div>${i}</div>`;
+            //loops through the events array
+            eventArray.forEach(function(event) {
+                //true if the event date matches the current date of the for loop
+                if(event.getDate(1) == i && (event.getDate(0) == date.getMonth() && event.getDate(2) == date.getFullYear())) {
+                    dailyEvents += event.getDescription() + "<br>";
+                }
+            })
+            days += `<div>${i + " " + `<p>${dailyEvents}</p>`}</div>`;
         }
     }
 
@@ -63,14 +114,29 @@ const renderCalendar = () => {
 
 }
 
+let event1 = new CalendarEvent("10/27/2021", "group", "a nice description1");
+let event2 = new CalendarEvent("10/27/2021", "group", "a nice description2");
+let event3 = new CalendarEvent("10/28/2021", "group", "a nice description3");
+let event4 = new CalendarEvent("10/2/2021", "group", "a nice description4");
+let event5 = new CalendarEvent("10/03/2021", "group", "a nice description5");
+let event6 = new CalendarEvent("10/04/2021", "group", "a nice description6");
+let event7 = new CalendarEvent("10/4/2021", "group", "a nice description7");
+let event8 = new CalendarEvent("11/2/2021", "group", "a nice description8");
+let event9 = new CalendarEvent("11/02/2021", "group", "a nice description9");
+let event10 = new CalendarEvent("11/13/2021", "group", "a nice description10");
+let event11 = new CalendarEvent("11/16/2021", "group", "a nice description11");
+let event12 = new CalendarEvent("11/25/2021", "group", "a nice description12");
+
+let eventArray = [event1, event2, event3, event4, event5, event6, event7, event8, event9, event10, event11, event12];
+
 document.querySelector('.prev').addEventListener('click', () => {
     date.setMonth(date.getMonth() - 1);
-    renderCalendar();
+    renderCalendar(eventArray);
 });
 
 document.querySelector('.next').addEventListener('click', () => {
     date.setMonth(date.getMonth() + 1);
-    renderCalendar();
+    renderCalendar(eventArray);
 });
 
-renderCalendar();
+renderCalendar(eventArray);
