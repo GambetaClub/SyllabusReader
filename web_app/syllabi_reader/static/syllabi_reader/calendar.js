82294afca8adb68b15131d7400db828d30430c31
ChrisHemstock <chris.hemstock@myemail.indwes.edu>
2021-11-13 19:17:05 -0500

    merged Chirs with main

--- web_app/syllabi_reader/static/syllabi_reader/calendar.js
+++ web_app/syllabi_reader/static/syllabi_reader/calendar.js
@@ -1,4 +1,6 @@
 //Heavily inspired by Code and Create https://www.youtube.com/watch?v=o1yMqPyYeAo&t=1135s
+
+//Class which defines events that will be added to the calendar
 class CalendarEvent {
 
     constructor(date, group, description) {
@@ -8,7 +10,7 @@ class CalendarEvent {
     }
    
     setDate(date) {
-        let dateArray = date.toString().split('/');
+        let dateArray = date.split('/')
         dateArray[0] = dateArray[0] - 1;
         this.date = dateArray;
     }
@@ -35,8 +37,26 @@ class CalendarEvent {
     
 }
 
+
+//constants used for rendering the calendar
 const date = new Date();
 const importButton = document.querySelector('.import');
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+//renders the calendar
 const renderCalendar = (eventArray) => {
     
     date.setDate(1);
@@ -53,21 +73,6 @@ const renderCalendar = (eventArray) => {
 
     const nextDays = 7 - lastDayIndex - 1;
 
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
-
     //displays the information in the header
     document.querySelector('.date h1').innerHTML = months[date.getMonth()];
 
@@ -81,7 +86,7 @@ const renderCalendar = (eventArray) => {
         days += `<div class = "prev-date">${prevLastDay - x + 1}</div>`
     }
 
-    //displays the days
+    //builds and displays the days for the month
     for(let i = 1; i <= lastDay; i++){
         let dailyEvents = "";
         //true if its todays date
@@ -93,7 +98,7 @@ const renderCalendar = (eventArray) => {
                         dailyEvents += event.getDescription() + "<br>";
                 }
             })
-            days += `<div class = "today">${i + " " + `<p>${dailyEvents}</p>`}</div>`;
+            days += `<div class = "${date.getFullYear()} ${months[date.getMonth()]} ${i} today">${i + " " + `<p>${dailyEvents}</p>`}</div>`;
             
         }else{
             //loops through the events array
@@ -103,7 +108,7 @@ const renderCalendar = (eventArray) => {
                     dailyEvents += event.getDescription() + "<br>";
                 }
             })
-            days += `<div>${i + " " + `<p>${dailyEvents}</p>`}</div>`;
+            days += `<div class = "${date.getFullYear()} ${months[date.getMonth()]} ${i}">${i + " " + `<p>${dailyEvents}</p>`}</div>`;
         }
     }
 
@@ -114,11 +119,10 @@ const renderCalendar = (eventArray) => {
 
 }
 
-let event1 = new CalendarEvent("10/27/2021", "group", "a nice description1");
+//Array used to store the events which will display on the calendar
+let eventArray = [];
 
-
-let eventArray = [event1];
-
+//checks for a click on the next and previous month arrows
 document.querySelector('.prev').addEventListener('click', () => {
     date.setMonth(date.getMonth() - 1);
     renderCalendar(eventArray);
@@ -129,9 +133,7 @@ document.querySelector('.next').addEventListener('click', () => {
     renderCalendar(eventArray);
 });
 
-document.querySelector('.import').addEventListener('click', () => {
-});
-
+//creates the calendar
 renderCalendar(eventArray);
 
 
@@ -139,11 +141,89 @@ renderCalendar(eventArray);
 const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]")
 
+//opens whenever a day is clicked on
 openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', function(e) {
+        e = e || window.event;
+        let target = e.target
+        let text = target.classList;
         const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
+        document.getElementById("title").innerHTML = text[1] + " " + text[2] + ", " + text[0]
+
+        //generates the text boxes and buttons on the popup for the specified day based on the events on that day
+        let dailyEvents = ""
+        let eventFound = false
+        eventArray.forEach(function(event, index) {
+            if(event.getDate(1) == text[2] && (event.getDate(0) == date.getMonth() && event.getDate(2) == date.getFullYear())) {
+                eventFound = true
+                //generates the textboxes and buttons for the specified event
+                dailyEvents += `<div class="event-section">
+                                    <b>Event</b>
+                                    <p>Date</p>
+                                    <input id="date${index}" type="text" value="${(event.getDate(0) + 1) + "/" + text[2] + "/" + event.getDate(2)}" />
+                                    <p>Group Name</p>
+                                    <input id="group${index}" type="text" value="${event.getGroup()}" />
+                                    <p>Description</p>
+                                    <input id="description${index}" type="text" value="${event.getDescription()}" />
+                                    <button data-event-delete${index} id="delete-event${index}" onclick="deleteEvent(${index}, eventArray)">Delete Event</button>
+                                </div>`
+            }
+        })
+        if(eventFound) {
+            dailyEvents += `<button data-event-save id="save-events">Save Events</button>`
+        }
+        //generates the add event textboxes and buttons
+        dailyEvents += `<div class="event-section">
+                            <b>Add Event</b>
+                            <p>Date</p>
+                            <input id="add-date" type="text" value="${(months.indexOf(text[1]) + 1) + "/" + text[2] + "/" + text[0]}"/>
+                            <p>Group Name</p>
+                            <input id="add-group" type="text"/>
+                            <p>Description</p>
+                            <input id="add-description" type="text"/>
+                            <button data-event-add id="add-event">Add Event</button>
+                        </div>`
+        document.getElementById("modal-body").innerHTML = dailyEvents;
+
+        //section that actually opens the modal
+        if(text[0] != "next-date" && text[0] != "prev-date" && text[0] != "days") {
+            console.log(text)
+            openModal(modal)
+
+            const createEventButton = document.querySelectorAll("[data-event-add]")
+            const saveEventButton = document.querySelectorAll("[data-event-save]")
+
+            createEventButton.forEach(button => {
+                button.addEventListener('click', () => {
+                    const date = document.getElementById("add-date").value
+                    console.log(date)
+                    const group = document.getElementById("add-group").value
+                    console.log(group)
+                    const description = document.getElementById("add-description").value
+                    console.log(description)
+                    addNewEvent(date, group, description, eventArray)
+                    renderCalendar(eventArray);
+                    const modal = button.closest('.modal')
+                    closeModal(modal)
+                })
+            })
+
+            saveEventButton.forEach(button => {
+                button.addEventListener('click', () => {
+                    eventArray.forEach(function(event, index) {
+                        if(event.getDate(1) == text[2] && (event.getDate(0) == date.getMonth() && event.getDate(2) == date.getFullYear())) {
+                            event.setDate(document.getElementById("date"+index).value)
+                            event.setGroup(document.getElementById("group"+index).value)
+                            event.setDescription(document.getElementById("description"+index).value)
+                            renderCalendar(eventArray);
+                            const modal = button.closest('.modal')
+                            closeModal(modal)
+                        }
+                    })
+                })
+            })
+        }
+    }, false);
 })
 
 closeModalButtons.forEach(button => {
@@ -179,7 +259,10 @@ function findEvent(date, group, description, array) {
 }
 
 function deleteEvent(index, array) {
-    array.splice(index)
+    console.log(index)
+    array.splice(index, 1)
+    renderCalendar(eventArray);
+    closeModal(document.getElementsByClassName("modal")[0])
 }
 
 function changeEvent(date, group, description, index, array) {
@@ -187,65 +270,6 @@ function changeEvent(date, group, description, index, array) {
         array[index].setGroup(group)
         array[index].setDescription(description)
 }
-
-const createEventButton = document.querySelectorAll("[data-event-add]")
-const deleteEventButton = document.querySelectorAll("[data-event-delete]")
-const changeEventButton = document.querySelectorAll("[data-event-change]")
-
-createEventButton.forEach(button => {
-    button.addEventListener('click', () => {
-        const date = document.getElementById("add-date").value
-        console.log(date)
-        const group = document.getElementById("add-group").value
-        console.log(group)
-        const description = document.getElementById("add-description").value
-        console.log(description)
-        addNewEvent(date, group, description, eventArray)
-        renderCalendar(eventArray);
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
-
-deleteEventButton.forEach(button => {
-    button.addEventListener('click', () => {
-        let date = document.getElementById("find-date").value.split('/')
-        date[0] = date[0]-1;
-        console.log("delete: " + date)
-        const group = document.getElementById("find-group").value
-        console.log("delete: " + group)
-        const description = document.getElementById("find-description").value
-        console.log("delete: " + description)
-        let index = findEvent(date, group, description, eventArray)
-        console.log(index)
-        if(index != null) {
-            deleteEvent(index, eventArray)
-            renderCal
-            const modal = button.closest('.modal')
-            closeModal(modal)
-        }
-    })
-})
-
-changeEventButton.forEach(button => {
-    button.addEventListener('click', () => {
-        let findDate = document.getElementById("find-date").value.split('/')
-        findDate[0] = findDate[0]-1;
-        const findGroup = document.getElementById("find-group").value
-        const findDescription = document.getElementById("find-description").value
-        let index = findEvent(findDate, findGroup, findDescription, eventArray)
-        console.log(index)
-        if(index != null) {
-            let changeDate = document.getElementById("change-date").value
-            const changeGroup = document.getElementById("change-group").value
-            const changeDescription = document.getElementById("change-description").value
-            changeEvent(changeDate, changeGroup, changeDescription, index, eventArray);
-            renderCalendar(eventArray);
-            const modal = button.closest('.modal')
-            closeModal(modal)
-        }
-    })
-})
 
 // It accepts a dictionary an iterates through its elements
 function handleResponse(obj) {  
